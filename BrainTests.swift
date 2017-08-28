2b7392e6eb5d8500e994c7e234e28a51d394414d
czhc <cedriczhchan@gmail.com>
2017-08-28 20:45:26 +0800

    Initial commit

---
+++ BrainTests.swift
@@ -0,0 +1,29 @@
+//
+//  BrainTests.swift
+//  FizzBuzz
+//
+//  Created by Cedric ZH Chan on 28/08/2017.
+//  Copyright © 2017 Cedric ZH Chan. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testIsDivisibleByThree(){
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
+}
---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,15 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Cedric ZH Chan on 28/08/2017.
+//  Copyright © 2017 Cedric ZH Chan. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    func isDivisibleByThree(number: Int) -> Bool {
+        return true
+    }
+}
