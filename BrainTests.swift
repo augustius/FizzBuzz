e58578ef87f37aa8c1a5c5090fb6cbe218bedf99
czhc <cedriczhchan@gmail.com>
2017-08-28 21:18:10 +0800

    Complete Brain

--- BrainTests.swift
+++ BrainTests.swift
@@ -10,7 +10,8 @@ import XCTest
 @testable import FizzBuzz
 
 class BrainTests: XCTestCase {
-    
+    let brain = Brain()
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,8 +23,53 @@ class BrainTests: XCTestCase {
     }
 
     func testIsDivisibleByThree(){
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 3)
         XCTAssertEqual(result, true)
     }
+    func testIsNotDivisibleByThree(){
+        let result = brain.isDivisibleByThree(number: 2)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive(){
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+
+    func testIsNotDivisibleByFive(){
+        let result = brain.isDivisibleByFive(number: 1)
+        XCTAssertEqual(result, false)
+    }
+
+    func testIsDivisibleByFifteen(){
+        let result = brain.isDivisibleByFifteen(number: 15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFifteen(){
+        let result = brain.isDivisibleByFifteen(number: 5)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testSayFizz(){
+        let result = brain.check(number: 3)
+        XCTAssertEqual(result, "Fizz")
+    }
+
+    func testSayBuzz(){
+        let result = brain.check(number: 5)
+        XCTAssertEqual(result, "Buzz")
+    }
+
+    func testSayFizzBuzz(){
+        let result = brain.check(number: 15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testSayNothing(){
+        let result = brain.check(number: 1)
+        XCTAssertEqual(result, "1")
+    }
+
+    
 }
